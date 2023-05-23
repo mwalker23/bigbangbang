b85b1348f1928097d24ee3a75a3ad8254617d022
Mona Lisa <madison0327@hotmail.com>
2023-05-22 20:35:31 -0400

    made front end form and did post request from the front end

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,34 @@
 console.log('hi');
+
+const userForm = document.getElementById('userForm')
+
+const formSubmission = async (e) => {
+    e.preventDefault();
+
+    const userValue = document.getElementById('userNameInput').value;
+    console.log(userValue);
+    const emailValue = document.getElementById('emailInput').value;
+    console.log(emailValue);
+    const passwordValue = document.getElementById('passwordInput').value;
+    console.log(passwordValue);
+
+
+    const postValue = await fetch('users/sign-up', {
+        method: "POST",
+        body: JSON.stringify({
+            username: userValue,
+            password: passwordValue,
+            email: emailValue
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    })
+
+    if (postValue.ok) {
+        alert("successful sign up!");
+    } else {
+        alert('fix your code!');
+    }
+}
+
+
+userForm.addEventListener('submit', formSubmission);
