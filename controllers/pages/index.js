86b90325673ed069dcf5214c4140643d36cf9684
Mona Lisa <madison0327@hotmail.com>
2023-05-22 21:22:36 -0400

    made dashboard and retrieved each user data

--- controllers/pages/index.js
+++ controllers/pages/index.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
-
+const { User } = require("../../models");
+// localhost:3001
 router.get('/', (req, res) => {
     res.render('homepage');
 })
+
+//localhost:3001/dashboard
+router.get('/dashboard/:user_id', async (req, res) => {
+    const userData = await User.findByPk(req.params.user_id, {
+        attributes: [
+            'username',
+            'email'
+        ]
+    });
+    console.log("this is ugly", userData);
+
+    const actualUserData = userData.get({ plain: true });
+    console.log("this is pretty", actualUserData);
+    res.render('dashboard', actualUserData);
+});
 
 module.exports = router;
